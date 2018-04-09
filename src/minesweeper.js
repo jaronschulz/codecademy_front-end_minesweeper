4418ab0b221bbb48fb54ae0620185b568c2a9111
jaronschulz <jaronschulz@gmail.com>
2018-04-09 20:49:44 +0200

    finished day 13 adding bomb board and playerboard drawing, started day 14 - interactivity

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,51 @@
-const printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+
+  for (iterateRows = 0; iterateRows < numberOfRows; iterateRows++) {
+    const row = [];
+    for (iterateColumns = 0; iterateColumns < numberOfColumns; iterateColumns++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
 
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+  for (iterateRows = 0; iterateRows < numberOfRows; iterateRows++) {
+    const row = [];
+    for (iterateColumns = 0; iterateColumns < numberOfColumns; iterateColumns++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);  
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+      const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      if (board[randomRowIndex][randomColumnIndex] === 'B') {
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+      }
+  }
+
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+// bombBoard will sometimes have less bombs than specified due to the previously-mentioned missing code.
+// Additionally, printing bombBoard will not look clean due to use of null instead of ' ' - this should just be for debugging, not presentation.
+printBoard(bombBoard);
